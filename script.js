d79b351518d44f17e77e9b4a6da9c89b537ec4f4
Michael Clark <michaelclarkwork@live.co.uk>
2023-12-18 15:25:04 +0000

    Addition of XML for data
    Added data XML file to hold info
    added JS functionality to populate info

---
+++ script.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      populateHTML(this);
+    }
+  };
+  xhttp.open("GET", "data.xml", true);
+  xhttp.send();
+};
+
+function populateHTML(xml) {
+  var xmlDoc = xml.responseXML;
+  document.getElementById("education").getElementsByTagName("p")[0].innerHTML =
+    xmlDoc.getElementsByTagName("education")[0].textContent;
+  document
+    .getElementById("workExperience")
+    .getElementsByTagName("p")[0].innerHTML =
+    xmlDoc.getElementsByTagName("workExperience")[0].textContent;
+  document.getElementById("skills").getElementsByTagName("p")[0].innerHTML =
+    xmlDoc.getElementsByTagName("skills")[0].textContent;
+}
